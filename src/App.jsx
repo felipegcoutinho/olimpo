be0b47787a4977123ce2ced0c01ab8726a9345f4
Felipe Coutinho <felipi@live.com>
2022-11-14 12:50:30 -0300

    layout tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, {useState} from "react";
+import Data from "./data.json";
+import "../src/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = React.useState("");
+  const filtered = query.length === 0;
+
+  const handleSearchChange = (e) => {
+    setQuery(e.target.value);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <input placeholder="Pesquise o produto" value={query} onChange={handleSearchChange} />
+
+      {filtered ? (
+        <div className="containernull">
+          <p>Pesquise</p>
+        </div>
+      ) : (
+        Data.filter((post) => {
+          if (post.modelo.toLowerCase().includes(query.toLowerCase())) {
+            return post;
+          } else if (post.manual_url.toLowerCase().includes(query.toLowerCase())) {
+            return post;
+          } else if (post.setor.toLowerCase().includes(query.toLowerCase())) {
+            return post;
+          }
+        }).map((post, index) => (
+          <div className="box" key={index}>
+            <div className="boxContent">
+              <p>{post.modelo}</p>
+              <p>{post.setor}</p>
+              <p>{post.manual_url}</p>
+            </div>
+          </div>
+        ))
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
