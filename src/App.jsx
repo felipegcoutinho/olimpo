7e680ee1e6077353b4529327170ed08c3b60997e
Felipe Coutinho <felipi@live.com>
2022-11-16 12:46:50 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Data from "./data.json";
-import "../src/App.css";
+import style from "../src/App.module.css";
+import {AP, CONVERSOR} from "./Header";
 
 function App() {
   const [query, setQuery] = React.useState("");
@@ -9,37 +10,123 @@ function App() {
   const handleSearchChange = (e) => {
     setQuery(e.target.value);
   };
+  const handlefibra = (e) => {
+    setQuery("fibra");
+  };
+  const hanldlempre = (e) => {
+    setQuery("empresariais");
+  };
+  const handleho = (e) => {
+    setQuery("home");
+  };
 
   return (
-    <>
-      <input placeholder="Pesquise o produto" value={query} onChange={handleSearchChange} />
+    <div className={style.container}>
+      <div className={style.search}>
+        <input
+          placeholder="Pesquise o equipamento"
+          value={query}
+          onChange={handleSearchChange}
+          className={style.searchbar}
+        />
+      </div>
+      <div className={style.btn_container}>
+        <button onClick={handlefibra}>Fibras</button>
+        <button onClick={hanldlempre}>Empresariais</button>
+        <button onClick={handleho}>Home Office</button>
+      </div>
 
       {filtered ? (
-        <div className="containernull">
+        <div className={style.noData}>
           <p>Pesquise</p>
         </div>
       ) : (
-        Data.filter((post) => {
-          if (post.modelo.toLowerCase().includes(query.toLowerCase())) {
-            return post;
-          } else if (post.manual_url.toLowerCase().includes(query.toLowerCase())) {
-            return post;
-          } else if (post.setor.toLowerCase().includes(query.toLowerCase())) {
-            return post;
+        Data.filter((device) => {
+          if (device.modelo.toLowerCase().includes(query.toLowerCase())) {
+            return device;
+          } else if (device.setor.toLowerCase().includes(query.toLowerCase())) {
+            return device;
           }
-        }).map((post, index) => (
-          <div className="box" key={index}>
-            <div className="boxContent">
-              <p>
-                <b>{post.modelo}</b>
-              </p>
-              <p>{post.setor}</p>
-              <a href={post.manual_url}>Manual</a>
+        }).map((device, index) => (
+          <div className={style.box_container} key={index}>
+            <div className={style.box_content}>
+              <h2>
+                <span className={style.setorh1}>{device.modelo}</span>
+                <span className={style.setorh3}>{device.setor}</span>
+              </h2>
+
+              <table className={style.GeneratedTable}>
+                {device.linha === "AP" && (
+                  <>
+                    <AP />
+                    <tbody>
+                      <tr>
+                        <td>{device.garantia}</td>
+                        <td>{device.cobertura}</td>
+                        <td>{device.raio}</td>
+                        <td>{device.usuarioMax}</td>
+                        <td>{device.porta}</td>
+                        <td>{device.throughputWireless24}</td>
+                        <td>{device.throughputWireless50}</td>
+                        <td>{device.qtdePortas}</td>
+                        <td>{device.PoE}</td>
+                        <td>{device.handover}</td>
+                        <td>{device.PotMaxTx}</td>
+                        <td>
+                          <span className={style.statusemlinha}>{device.status}</span>
+                        </td>
+
+                        <td>
+                          <a href={device.pagina}>Página</a>
+                        </td>
+                        <td>
+                          <a href={device.datashet_url}>Datasheet</a>
+                        </td>
+                        <td>
+                          <a href={device.guia}>Guia</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </>
+                )}
+                {device.linha === "CONVERSOR" && (
+                  <>
+                    <CONVERSOR />
+                    <tbody>
+                      <tr>
+                        <td>{device.conector}</td>
+                        <td>{device.wdm}</td>
+                        <td>{device.distancia}</td>
+                        <td>{device.modulação}</td>
+                        <td>{device.tipo_fibra}</td>
+                        <td>{device.potencia_sinal}</td>
+                        <td>{device.compTx}</td>
+                        <td>{device.compRx}</td>
+                        <td>
+                          <span className={device.status === "Em Linha" ? style.statusemlinha : style.statusphaseout}>
+                            {device.status}
+                          </span>
+                        </td>
+                        <td>{device.garantia}</td>
+                        <td>
+                          <a href={device.pagina}>Página</a>
+                        </td>
+                        <td>
+                          <a href={device.datashet_url}>Datasheet</a>
+                        </td>
+                        <td>
+                          <a href={device.guia}>Guia</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </>
+                )}
+              </table>
             </div>
           </div>
         ))
       )}
-    </>
+    </div>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export function AP() {
+  return (
+    <>
+      <thead>
+        <tr>
+          <th>Garantia</th>
+          <th>Cobertura</th>
+          <th>Raio</th>
+          <th>Usuários simultâneos</th>
+          <th>Porta</th>
+          <th>Wireless 2.4ghz</th>
+          <th>Wireless 5ghz</th>
+          <th>Qtde Portas</th>
+          <th>PoE</th>
+          <th>Handover</th>
+          <th>PotMaxTx</th>
+          <th>Status</th>
+          <th>Página</th>
+          <th>Datasheet</th>
+          <th>Guia</th>
+        </tr>
+      </thead>
+    </>
+  );
+}
+
+export function CONVERSOR() {
+  return (
+    <>
+      <thead>
+        <tr>
+          <th>Conector</th>
+          <th>WDM</th>
+          <th>Distância</th>
+          <th>Modulação</th>
+          <th>Fibra</th>
+          <th>Potencia Sinal</th>
+          <th>Comprimento TX</th>
+          <th>Comprimento RX</th>
+          <th>Status</th>
+          <th>Garantia</th>
+          <th>Página</th>
+          <th>Datasheet</th>
+          <th>Guia</th>
+        </tr>
+      </thead>
+    </>
+  );
+}
