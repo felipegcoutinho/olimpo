250aa1563bdc9a86149034076e2cee31cff08656
Felipe Coutinho <felipi@live.com>
2022-11-18 20:48:41 -0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,131 +1,135 @@
 import React, {useState} from "react";
 import Data from "./data.json";
 import style from "../src/App.module.css";
-import {AP, CONVERSOR} from "./Header";
+import {AP, CONVERSOR, SWITCH} from "./Header";
 
 function App() {
-  const [query, setQuery] = React.useState("");
-  const filtered = query.length === 0;
-
-  const handleSearchChange = (e) => {
-    setQuery(e.target.value);
-  };
-  const handlefibra = (e) => {
-    setQuery("fibra");
-  };
-  const hanldlempre = (e) => {
-    setQuery("empresariais");
-  };
-  const handleho = (e) => {
-    setQuery("home");
-  };
-
   return (
     <div className={style.container}>
-      <div className={style.search}>
-        <input
-          placeholder="Pesquise o equipamento"
-          value={query}
-          onChange={handleSearchChange}
-          className={style.searchbar}
-        />
-      </div>
       <div className={style.btn_container}>
-        <button onClick={handlefibra}>Fibras</button>
-        <button onClick={hanldlempre}>Empresariais</button>
-        <button onClick={handleho}>Home Office</button>
+        <a href="#ap">
+          <button>Access Point</button>
+        </a>
+        <a href="#radios">
+          <button>Radios</button>
+        </a>
+        <a href="#switch">
+          <button>Switch</button>
+        </a>
+        <a href="#conversor">
+          <button>Conversor</button>
+        </a>
+        <a href="#olt">
+          <button>OLT</button>
+        </a>
+        <a href="#olt">
+          <button>Roteadores</button>
+        </a>
       </div>
 
-      {filtered ? (
-        <div className={style.noData}>
-          <p>Pesquise</p>
+      <div className={style.box_container}>
+        <div className={style.box_content}>
+          <h2>Access Point</h2>
+          <table className={style.GeneratedTable} id="ap">
+            <AP />
+            {Data.map((device) => {
+              if (device.linha === "ap") {
+                return (
+                  <tbody>
+                    <tr>
+                      <td>
+                        <b>{device.modelo}</b>
+                      </td>
+                      <td>{device.cobertura}</td>
+                      <td>{device.raio}</td>
+                      <td>{device.usuarioMax}</td>
+                      <td>{device.porta}</td>
+                      <td>{device.throughputWireless24}</td>
+                      <td>{device.throughputWireless50}</td>
+                      <td>{device.qtdePortas}</td>
+                      <td>{device.PoE}</td>
+                      <td>{device.handover}</td>
+                      <td>{device.PotMaxTx}</td>
+                      <td>{device.status}</td>
+                      <td>
+                        <a href={device.pagina}>pagina</a>
+                      </td>
+                      <td>
+                        <a href={device.datashet_url}>datasheet</a>
+                      </td>
+                      <td>
+                        <a href={device.guia}>guia</a>
+                      </td>
+                    </tr>
+                  </tbody>
+                );
+              }
+            })}
+          </table>
         </div>
-      ) : (
-        Data.filter((device) => {
-          if (device.modelo.toLowerCase().includes(query.toLowerCase())) {
-            return device;
-          } else if (device.setor.toLowerCase().includes(query.toLowerCase())) {
-            return device;
-          }
-        }).map((device, index) => (
-          <div className={style.box_container} key={index}>
-            <div className={style.box_content}>
-              <h2>
-                <span className={style.setorh1}>{device.modelo}</span>
-                <span className={style.setorh3}>{device.setor}</span>
-              </h2>
-
-              <table className={style.GeneratedTable}>
-                {device.linha === "AP" && (
-                  <>
-                    <AP />
-                    <tbody>
-                      <tr>
-                        <td>{device.garantia}</td>
-                        <td>{device.cobertura}</td>
-                        <td>{device.raio}</td>
-                        <td>{device.usuarioMax}</td>
-                        <td>{device.porta}</td>
-                        <td>{device.throughputWireless24}</td>
-                        <td>{device.throughputWireless50}</td>
-                        <td>{device.qtdePortas}</td>
-                        <td>{device.PoE}</td>
-                        <td>{device.handover}</td>
-                        <td>{device.PotMaxTx}</td>
-                        <td>
-                          <span className={style.statusemlinha}>{device.status}</span>
-                        </td>
-
-                        <td>
-                          <a href={device.pagina}>Página</a>
-                        </td>
-                        <td>
-                          <a href={device.datashet_url}>Datasheet</a>
-                        </td>
-                        <td>
-                          <a href={device.guia}>Guia</a>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </>
-                )}
-                {device.linha === "CONVERSOR" && (
-                  <>
-                    <CONVERSOR />
-                    <tbody>
-                      <tr>
-                        <td>{device.conector}</td>
-                        <td>{device.wdm}</td>
-                        <td>{device.distancia}</td>
-                        <td>{device.modulação}</td>
-                        <td>{device.tipo_fibra}</td>
-                        <td>{device.potencia_sinal}</td>
-                        <td>{device.compTx}</td>
-                        <td>{device.compRx}</td>
-                        <td>
-                          <span className={device.status === "Em Linha" ? style.statusemlinha : style.statusphaseout}>
-                            {device.status}
-                          </span>
-                        </td>
-                        <td>{device.garantia}</td>
-                        <td>
-                          <a href={device.pagina}>Página</a>
-                        </td>
-                        <td>
-                          <a href={device.datashet_url}>Datasheet</a>
-                        </td>
-                        <td>
-                          <a href={device.guia}>Guia</a>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </>
-                )}
-              </table>
-            </div>
-          </div>
-        ))
-      )}
+        <div className={style.box_content}>
+          <h2>Conversor de Mídia</h2>
+          <table className={style.GeneratedTable} id="conversor">
+            <CONVERSOR />
+            {Data.map((device) => {
+              if (device.linha === "conversor") {
+                return (
+                  <tbody>
+                    <tr>
+                      <td>{device.modelo}</td>
+                      <td>{device.cobertura}</td>
+                      <td>{device.raio}</td>
+                      <td>{device.Usuários_simultâneos}</td>
+                      <td>{device.porta}</td>
+                      <td>{device.Wireless_2_4ghz}</td>
+                      <td>{device.Wireless_5ghz}</td>
+                      <td>{device.compTxQtde_Portas}</td>
+                      <td>{device.PoE}</td>
+                      <td>{device.handover}</td>
+                      <td>{device.PotMaxTx}</td>
+                      <td>{device.status}</td>
+                      <td>{device.pagina}</td>
+                      <td>{device.Datasheet}</td>
+                      <td>{device.guia}</td>
+                    </tr>
+                  </tbody>
+                );
+              }
+            })}
+          </table>
+        </div>
+        <div className={style.box_content}>
+          <h2>Switch</h2>
+          <table className={style.GeneratedTable} id="switch">
+            <SWITCH />
+            {Data.map((device) => {
+              if (device.linha === "switch") {
+                return (
+                  <tbody>
+                    <tr>
+                      <td>{device.modelo}</td>
+                      <td>{device.cobertura}</td>
+                      <td>{device.raio}</td>
+                      <td>{device.Usuários_simultâneos}</td>
+                      <td>{device.porta}</td>
+                      <td>{device.Wireless_2_4ghz}</td>
+                      <td>{device.Wireless_5ghz}</td>
+                      <td>{device.compTxQtde_Portas}</td>
+                      <td>{device.PoE}</td>
+                      <td>{device.handover}</td>
+                      <td>{device.PotMaxTx}</td>
+                      <td>{device.status}</td>
+                      <td>{device.pagina}</td>
+                      <td>{device.Datasheet}</td>
+                      <td>{device.guia}</td>
+                    </tr>
+                  </tbody>
+                );
+              }
+            })}
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
