821474b315709f75091c9121a7e22e381ed9c82e
Felipe Coutinho <felipi@live.com>
2022-11-28 11:27:09 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -252,7 +252,7 @@ function App() {
             <h2 id="switch">Switchs</h2>
             <label>
               <i className="fa-solid fa-magnifying-glass"></i>
-              <input placeholder="Pesquise o Switch" value={querySWITCH} onChange={handleSearchChangeSWITCH} className={style.searchbar} />
+              <input className={style.searchbar} placeholder="Pesquise o Switch" value={querySWITCH} onChange={handleSearchChangeSWITCH} />
             </label>
           </div>
 
